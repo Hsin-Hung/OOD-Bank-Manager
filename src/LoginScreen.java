78a35f8b858d0ba5db01dfdcb908997052359b69
Hsin-Hung Wu <henrywu19970511@gmail.com>
2020-12-07 16:16:40 +0800

    modify loginscreen

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -109,7 +109,7 @@ public class LoginScreen extends BaseScreen {
         label3.setText("LoginScreen Form");
         mainPanel.add(label3, new GridConstraints(2, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         createUserButton = new JButton();
-        createUserButton.setText("CreateUser");
+        createUserButton.setText("CreateNewUser");
         mainPanel.add(createUserButton, new GridConstraints(6, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer5 = new Spacer();
         mainPanel.add(spacer5, new GridConstraints(9, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
