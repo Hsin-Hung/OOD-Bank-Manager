0e6072794d001c3e55367b5633f25f1eb1ed2ce7
Eric Chen <ericjyc@bu.edu>
2020-12-16 22:15:08 +0800

    add limit to transfer dialog
    Signed-off-by: Eric Chen <ericjyc@bu.edu>

--- src/TransferDialog.java
+++ src/TransferDialog.java
@@ -23,6 +23,9 @@ public class TransferDialog extends JDialog {
         this.atm = atm;
         this.owner = owner;
 
+        transferAmount.setModel(new SpinnerNumberModel(0, 0.01, Math.max(5.0, owner.getAccount().getBalance().doubleValue()), 0.01));
+        Helper.disableSpinnerInvalids(transferAmount);
+        
         $$$setupUI$$$();
         setContentPane(contentPane);
         setModal(true);
